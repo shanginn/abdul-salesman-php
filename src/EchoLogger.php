b6a6136391d47a483c5b020f7e735badc50892f6
Nikolai Shangin <shanginn@gmail.com>
2024-07-26 20:23:50 +0700

    Refactor EchoLogger to improve type hinting for the log method, enhancing code clarity and type safety.

--- src/EchoLogger.php
+++ src/EchoLogger.php
@@ -1,13 +1,17 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Shanginn\AbdulSalesman;
 
 use Psr\Log\AbstractLogger;
+use Stringable;
 
-class EchoLogger extends AbstractLogger {
-    public function log($level, string|\Stringable $message, array $context = []): void {
-        echo "[$level] $message ";
+class EchoLogger extends AbstractLogger
+{
+    public function log($level, string|Stringable $message, array $context = []): void
+    {
+        echo "[{$level}] {$message} ";
 
         if (!empty($context)) {
             echo json_encode($context);
